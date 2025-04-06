db53d6851a85a18f33a88a50ec49a8084b2ea091
André Meira <msandredev@gmail.com>
2025-04-06 10:14:37 -0300

    first commit

---
+++ src/test/java/br/com/msandredev/hubspot_integration_api/HubspotIntegrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msandredev.hubspot_integration_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubspotIntegrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
